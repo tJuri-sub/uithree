b570edb708789c089cb58eefe175904f3346b5d2
juri turiano <juricuber@gmail.com>
2024-07-11 21:35:52 +0800

    a

--- thrattempt/src/components/icons.jsx
+++ thrattempt/src/components/icons.jsx
@@ -4,7 +4,7 @@ export const InfoIcon = (props) => (
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    style="fill: rgba(255, 87, 51, 1);transform: ;msFilter:;"
+    style={{ color: "rgba(255, 87, 51, 1)" }}
     {...props}
   >
     <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path>
